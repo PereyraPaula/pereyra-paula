cd10b0155da6cd6e5a17f8dac02844cf951168a0
PereyraPaula <paulapereyrallorens@gmail.com>
2026-03-24 13:48:02 -0300

    Astro Build performance optimized - Images optimized

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,24 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  build: {
+    // Habilita la inserción de recursos en línea para archivos pequeños
+    inlineStylesheets: 'auto',
+  },
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [
+      tailwindcss(),
+      compress({
+        CSS: true,
+        HTML: true,
+        JavaScript: true,
+        Image: false,
+      })
+    ],
   },
 });
